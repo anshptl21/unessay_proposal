f1b728c8cbbfa571a0908a5d941e45aced0a90fe
Ansh Patel <85411358+anshptl21@users.noreply.github.com>
2024-12-02 13:00:40 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-function toggleElement(elementId) {
+function toggleElement(elementId, elementButton) {
     // Get all elements with the class 'slide-element'
     const slideElements = document.querySelectorAll('.slide-element');
-  
+    const buttons = document.querySelectorAll('.button');
+    console.log(buttons)
     // Hide all elements with the slideOut animation
     slideElements.forEach(element => {
       element.style.animation = 'slideOut 1.2s ease-in-out';
@@ -9,9 +10,19 @@ function toggleElement(elementId) {
         element.style.display = 'none'
       }, 1200);
     });
+
+    buttons.forEach(element => {
+        element.style.backgroundColor = 'transparent';
+        element.style.color = 'darksalmon';
+        element.style.borderRadius = '0';
+      });
   
     // Show the selected element with the slideIn animation
     const selectedElement = document.getElementById(elementId);
+    const selectedButton = document.getElementById(elementButton);
+    selectedButton.style.backgroundColor = 'black'
+    selectedButton.style.color = 'antiquewhite'
+    selectedButton.style.borderRadius = '10%'
     selectedElement.style.animation = 'slideIn 1.2s ease-in-out';
     setTimeout(() => {
         selectedElement.style.display = 'block';
@@ -21,3 +32,39 @@ function toggleElement(elementId) {
 
 
 
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const bText = entry.target.querySelector('.before')
+      const aText = entry.target.querySelector('.after')
+      const image = entry.target.querySelector('.after_window')
+      if (entry.isIntersecting) {
+        image.classList.add('slid')
+        image.classList.add('display')
+        setTimeout(() => {
+        bText.classList.add('fadeC')
+        aText.classList.add('fadeC')   
+        }, 1201);
+        setTimeout(() =>{
+            bText.classList.add('color')
+            aText.classList.add('color')
+        }, 1503);
+      }
+    });
+  });
+  
+const ob2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const title = entry.target.querySelector('.parallax')
+      console.log(title)
+      if (entry.isIntersecting) {
+        title.classList.add('scaled')
+      }
+    });
+  });
+  
+
+
+
+
+observer.observe(document.getElementById('image'));
+ob2.observe(document.getElementById('paral'));
